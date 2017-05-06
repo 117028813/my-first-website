3eb8be9232e4605b7e0b6c909484170f8a035cf0
ljy <117028813@qq.com>
2017-05-06 10:05:34 +0800

    debug rating parseFloat and toFix. move movies in movies.json file.

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 let express = require('express')
 let app = express()
 let bodyParser = require('body-parser')
+let fs = require('fs')
 
+// let movies = [
+//   {id: 1, name: '速度与激情8', rating: 7.2},
+//   {id: 2, name: '攻壳机动队', rating: 6.6},
+//   {id: 3, name: '金刚狼：殊死一战', rating: 8.3}
+// ]
+let movies
 
-let movies = [
-  {id: 1, name: '速度与激情8', rating: 7.2},
-  {id: 2, name: '攻壳机动队', rating: 6.6},
-  {id: 3, name: '金刚狼：殊死一战', rating: 8.3}
-]
+fs.readFile('./movies.json', 'utf8', (err, data) => {
+  if (err) {
+    throw err
+  } else {
+    movies = JSON.parse(data)
+  }
+})
 
 
 app.use(express.static('public'))
@@ -31,15 +40,23 @@ app.get('/movies/:id', function (req, res) {
   } else {
     movies.splice(removeIndex, 1)
     res.send(movies)
+    fs.writeFile('./movies.json', JSON.stringify(movies), (err) => {
+      if (err) throw err
+      console.log('The file has been saved')
+    })
   }
 })
 
 
 app.post('/movies/add', function (req, res) {
   let id = movies[movies.length-1].id + 1
-  let movie = {id: id, name: req.body.name, rating: parseInt(req.body.rating)}
+  let movie = {id: id, name: req.body.name, rating: parseFloat(req.body.rating)}
   movies.push(movie)
   res.send(movies)
+  fs.writeFile('./movies.json', JSON.stringify(movies), (err) => {
+    if (err) throw err
+    console.log('The file has been saved')
+  })
 })
 
 
